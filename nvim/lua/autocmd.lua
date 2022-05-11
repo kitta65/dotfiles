8ef9d65bfd3ce66a13c8f17aaf3de4200a37689b
dr666m1 <skndr666m1@gmail.com>
2022-05-11 23:55:19 +0900

    Add bigquery command (for the time being)

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -13,6 +13,9 @@ vim.api.nvim_create_autocmd({"FileType"}, {
 group = vim.api.nvim_create_augroup("bigquery", {})
 cmds = {
   [[setlocal foldmethod=indent]],
+  [[command! BQUpdateCache lua vim.lsp.buf_request(0, "bq/updateCache", nil, function(err) if err print(err) end)]],
+  [[command! BQClearCache lua vim.lsp.buf_request(0, "bq/clearCache", nil, function(err) if err print(err) end)]],
+  [[command! BQDryRun lua vim.lsp.buf_request(0, "bq/dryRun", {"uri": "file://" . expand("%:p")}, function(err) if err print(err) end)]],
 }
 for _, cmd in ipairs(cmds) do
   vim.api.nvim_create_autocmd({"FileType"}, {
