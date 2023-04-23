409f3b409b55fc9693fd36c57f9213f9c49f354e
dr666m1 <skndr666m1@gmail.com>
2023-04-23 18:59:41 +0900

    use null-ls

--- astronvim/init.lua
+++ astronvim/init.lua
@@ -18,11 +18,30 @@ return {
         require("luasnip.loaders.from_snipmate").lazy_load({ paths = { "~/dotfiles/astronvim/snippets" } })
       end,
     },
+    {
+      "jose-elias-alvarez/null-ls.nvim",
+      opts = function(_, config)
+        local null_ls = require "null-ls"
+        config.sources = {
+          null_ls.builtins.formatting.prettier.with { only_local = "node_modules/.bin" },
+        }
+        return config
+      end,
+    },
     { "rafamadriz/friendly-snippets", enabled = false },
     { "max397574/better-escape.nvim", enabled = false },
     { "windwp/nvim-autopairs",        enabled = false },
   },
   lsp = {
     servers = { "bqls" },
+    -- https://astronvim.com/Recipes/advanced_lsp#disabling-formatting-for-a-filter-function
+    formatting = {
+      filter = function(client)
+        if vim.bo.filetype == "typescript" then
+          return client.name == "null-ls"
+        end
+        return true
+      end
+    }
   },
 }
