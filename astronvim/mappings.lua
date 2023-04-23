79c33520dc6d6ccb9bd8da674fb1efb8f60954b1
dr666m1 <skndr666m1@gmail.com>
2023-04-23 15:37:40 +0900

    wip

--- astronvim/mappings.lua
+++ astronvim/mappings.lua
@@ -1,41 +1,44 @@
 local keys = {
-  i = {
-    ["jk"] = { "<esc>", desc = "escape" },
-    ["<c-j>"] = { "<down>", desc = "↓" },
-    ["<c-k>"] = { "<up>", desc = "↑" },
-    ["<c-l>"] = { "<right>", desc = "→" },
-  },
-  v = {
-    ["v"] = { "<esc>", desc = "escape" }
-  },
+	i = {
+		["jk"] = { "<esc>", desc = "escape" },
+		["<c-j>"] = { "<down>", desc = "↓" },
+		["<c-k>"] = { "<up>", desc = "↑" },
+		["<c-l>"] = { "<right>", desc = "→" },
+	},
+	v = {
+		["v"] = { "<esc>", desc = "escape" }
+	},
 }
 
 local quotes = {
-  { "'" },
-  { "'''" },
-  { '"' },
-  { '"""' },
-  { '|' },
-  { '_' },
-  { '__' },
-  { '*' },
-  { '**' },
-  { '`' },
-  { '```' },
-  { "[",  "]" },
-  { "<",  ">" },
-  { "{",  "}" },
-  { "{{", "}}" },
-  { "/*", "*/" },
+	{ "'" },
+	{ "'''" },
+	{ '"' },
+	{ '"""' },
+	{ '|' },
+	{ '_' },
+	{ '__' },
+	{ '*' },
+	{ '**' },
+	{ '`' },
+	{ '```' },
+	{ "[",  "]" },
+	{ "<",  ">" },
+	{ "{",  "}" },
+	{ "{{", "}}" },
+	{ "/*", "*/" },
 }
 for i = 1, #quotes do
-  local l = quotes[i][1]
-  local r = quotes[i][2]
-  if not r then
-    r = l
-  end
-  keys.i[l .. "<tab>"] = { r .. string.rep("<left>", #r) .. l }
-  keys.v[l] = { "c" .. l .. "<c-r>0" .. r }
+	local l = quotes[i][1]
+	local r = quotes[i][2]
+	if not r then
+		r = l
+	end
+	keys.i[l .. "<tab>"] = { r .. string.rep("<left>", #r) .. l }
+	keys.v[l] = { function()
+		-- vim.opt.paste = true
+		return "c" .. l .. "<c-r>0" .. r .. "<esc>:set nopaste<cr>"
+	end }
 end
 
 return keys
