69fefb97e0a4a0925282c1c73c24cf634d4d493a
dr666m1 <skndr666m1@gmail.com>
2023-04-23 13:12:05 +0900

    snippets

--- astronvim/init.lua
+++ astronvim/init.lua
@@ -2,9 +2,17 @@ return {
   colorscheme = "terafox",
   plugins = {
     "EdenEast/nightfox.nvim",
-    -- https://github.com/AstroNvim/AstroNvim/issues/1250
+    -- https://astronvim.com/Recipes/snippets
+    {
+      "L3MON4D3/LuaSnip",
+      config = function(plugin, opts)
+        -- https://astronvim.com/Recipes/custom_plugins#extending-core-plugin-config-functions
+        require "plugins.configs.luasnip" (plugin, opts)
+        require("luasnip.loaders.from_snipmate").lazy_load({ paths = { "~/dotfiles/astronvim/snippets" } })
+      end,
+    },
+    { "rafamadriz/friendly-snippets", enabled = false },
     { "max397574/better-escape.nvim", enabled = false },
-    -- https://github.com/AstroNvim/AstroNvim/issues/1574
     { "windwp/nvim-autopairs",        enabled = false },
   }
 }
