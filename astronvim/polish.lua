e239face8d1a57837e976f4a105d1363534c5599
dr666m1 <skndr666m1@gmail.com>
2023-05-24 23:51:14 +0900

    show control characters

--- astronvim/polish.lua
+++ astronvim/polish.lua
@@ -1,1 +1,12 @@
 require "user.autocmds"
+
+vim.opt.list = true
+vim.opt.listchars = {
+  tab = '»-',
+  lead = '.',
+  trail = '.',
+  eol = '↲',
+  extends = '»',
+  precedes = '«',
+	nbsp = '.'
+}
