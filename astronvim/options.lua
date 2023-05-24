edab5623815cf4ea9d2b8f648bfcd11cf0024ce0
dr666m1 <skndr666m1@gmail.com>
2023-05-25 00:04:58 +0900

    refactored

---
+++ astronvim/options.lua
@@ -0,0 +1,14 @@
+return {
+  opt = {
+    list = true,
+    listchars = {
+      tab = '» ',
+      lead = '.',
+      trail = '.',
+      eol = '↲',
+      extends = '»',
+      precedes = '«',
+      nbsp = '.',
+    },
+  },
+}
--- astronvim/polish.lua
+++ astronvim/polish.lua
@@ -1,12 +1,1 @@
 require "user.autocmds"
-
-vim.opt.list = true
-vim.opt.listchars = {
-  tab = '» ',
-  lead = '.',
-  trail = '.',
-  eol = '↲',
-  extends = '»',
-  precedes = '«',
-  nbsp = '.',
-}
